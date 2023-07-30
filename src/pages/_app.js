913c00e379d1ce29d47d3802633814bd8e67c6ed
Trunova Viktoria <vikit2000@ukr.net>
2023-07-31 00:10:58 +0300

    initialize project

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import 'normalize.css/normalize.css'
+import '@/styles/globals.css'
+//npm install styled-components@5.3.0
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
